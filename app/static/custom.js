d7067038e9a839168a1c9143c6e438af30e9cedf
johannes-gehrs <jgehrs@gmail.com>
2014-01-08 02:03:50 +0100

    various layout impovements wobbling logo

--- app/static/custom.js
+++ app/static/custom.js
@@ -1,9 +1,20 @@
-//Register FastClick
+// Register FastClick
 $(function () {
     FastClick.attach(document.body);
 });
 
 $(document).ready(function () {
+    // Show active Nav element
     var link_selector = 'a[href="' + window.location.pathname + '"]';
     $('ul#nav_ul').find(link_selector).parent().addClass('active');
+
+    // Animate Logo
+    var animation_classes = 'animated wobble'
+    var $logo = $('#logo');
+    $logo.click(function () {
+        $logo.addClass(animation_classes);
+        $logo.one('webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd', function () {
+            $logo.removeClass(animation_classes);
+        });
+    });
 });
