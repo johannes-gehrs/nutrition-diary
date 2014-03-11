4502c01faf59e6b5dce238b9c706c1c4d16b69a6
johannes-gehrs <jgehrs@gmail.com>
2014-03-11 22:12:16 +0100

    adds proper CSRF protection for AJAX calls.
    CSRF protection had been deactivated before. It was activated with a different commit, unfortunately.

--- app/static/custom.js
+++ app/static/custom.js
@@ -13,22 +13,44 @@ $(document).ready(function () {
     var $logo = $('#logo');
     $logo.click(function () {
         $logo.addClass(animation_classes);
+        // If you don't do the following, you wobble once and you're done forever
         $logo.one('webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd', function () {
             $logo.removeClass(animation_classes);
         });
     });
 });
 
+// Aquire CSRF-Token from cookie
+// See https://docs.djangoproject.com/en/dev/ref/contrib/csrf/#ajax
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
 delete_serving = function (url, serving_id) {
     jqxhr = $.ajax({
         url: url,
-        type: 'DELETE'
+        type: 'DELETE',
+        crossDomain: false,
+        headers: {'X-CSRFToken': csrftoken}
     });
     jqxhr.success(function () {
         console.log('Successfully deleted: ' + serving_id);
         location.reload();
     });
     jqxhr.error(function () {
-        console.log("Delete request didn't work: " +  serving_id)
+        console.log("Delete request didn't work: " + serving_id)
     });
 };
